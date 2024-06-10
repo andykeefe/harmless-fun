f74ecde19127643d6008fcbf44dfa7a3bb166599
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-10 15:55:10 -0500

    Update latency_uncommented.py

--- latency_uncommented.py
+++ latency_uncommented.py
@@ -2,7 +2,6 @@ from subprocess import check_output
 from time import sleep, strftime
 from datetime import date
 
-
 def pinger(target_host, max_tests=11):
     count = 1
     total_latency = 0
@@ -11,8 +10,12 @@ def pinger(target_host, max_tests=11):
         while count < max_tests:
             output = check_output(['ping', '-c', '1', target_host], universal_newlines=True)
             # output = check_output(['ping', '-n', '1', target_host], universal_newlines=True)
+            # ^^ If on Windows, uncomment line above, and comment out two lines abov.
+            
             latency = output.split("time=")[-1].split(" ")[0]
             # latency = output.split('time=")[-1].split("ms")[0]
+            # ^^ If on Windows, uncomment line above, and comment out two lines abov.
+            
             total_latency += float(latency)
             print(f"Test #{count} --> Latency: {latency} ms")
             count += 1
@@ -30,15 +33,14 @@ def pinger(target_host, max_tests=11):
         f.write(f"Date: {strftime('%m/%d/%Y')}\nTtarget: {target_host}")
         f.write(f"\nAverage latency: {average_latency} ms\n")
 
-
 if __name__ == '__main__':
+    default = "fountaindale.org"
     target = input("Y/n: Specify a target address?\n")
     
     if target.lower() in('y', 'yes'):
         to_be_pinged = input("Enter an address to be pinged: ")
         print(f"\nTarget: {to_be_pinged}")
         pinger(to_be_pinged)
-
     else:
-        print("Default target: fountaindale.org")
-        pinger("fountaindale.org")
+        print(f"Default target: {default}")
+        pinger(default)
