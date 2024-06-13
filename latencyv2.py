656ff987e32fdbc400b3d7ad6c1a22bd71adf8e5
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-13 17:24:39 -0500

    Update latencyv2.py

--- latencyv2.py
+++ latencyv2.py
@@ -13,19 +13,19 @@ def pinger(target_host, max_tests=101):
             try:
                 output = check_output(['ping', '-c', '1', target_host], universal_newlines=True)
                 
-                # ping test above is properly formatted for Mac and Linux. If using Windows,
-                # comment out line 14 and use this instead:
+                """ ping test above is properly formatted for Mac and Linux. If using Windows,
+                comment out line 14 and use this instead: """
                 # output = check_output(['ping', '-n', '1', target_host], universal_newlines=True)
                 
                 latency = output.split("time=")[-1].split(" ")[0]
                 
-                # Extracts numeric latency value and strips everything else
+                """ Extracts numeric latency value and strips everything else
                 
-                # Line above works on Mac and Linux b/c they separate latency value 
-                # and unit with a space, i.e. time=16 ms. This is not the case on Windows,
-                # which uses the format time=16ms. 
+                Line above works on Mac and Linux b/c they separate latency value 
+                and unit with a space, i.e. time=16 ms. This is not the case on Windows,
+                which uses the format time=16ms. 
                 
-                # If using Windows, use the following line:
+                # If using Windows, use the following line:  """
                 # latency = output.split("time=")[-1].split("ms")[0]
                 
                 total_latency += float(latency)
@@ -41,11 +41,10 @@ def pinger(target_host, max_tests=101):
                 print("---------------------------------------\n")
                 exit(1)
                 
-                #  Instead of printing the lengthy error message when an
-                #  address is not found, we just print the target host.
+                """  Instead of printing the lengthy error message when an
+                     address is not found, we just print the target host.   """
 
-            average_latency_float = total_latency / (max_tests - 1)
-            average_latency = round(average_latency_float, 1)
+            average_latency = round(total_latency / (max_tests - 1), 1)
         
         if count == max_tests:
             print(f"\nAverage latency: ~{average_latency} ms")
@@ -57,9 +56,9 @@ def pinger(target_host, max_tests=101):
         f.write(f"Date: {strftime('%m/%d/%Y')}\nTo target: {target_host}")
         f.write(f"\nAverage latency: {average_latency} ms\n")
        
-        #  Writes the average latency result to a file tests.txt, appending
-        #  each successive test. So tests.txt displays 1. The date the test
-        #  occurred  2. The target host  3. The average latency
+        """ Writes the average latency result to a file tests.txt, appending
+            each successive test. So tests.txt displays 1. The date the test
+            occurred  2. The target host  3. The average latency     """
 
 if __name__ == '__main__':
     default_target = "fountaindale.org"
